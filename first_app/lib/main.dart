e0f0ea70931a2f7d5f5334384059b4cb7fef876b
nahoshi002 <anhhaonguyen002@gmail.com>
2023-10-27 22:30:02 +0700

    Update instagram ui

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,22 +1,24 @@
-import 'package:first_app/my_cinema.dart';
-import 'package:first_app/my_class_room.dart';
-import 'package:first_app/my_form.dart';
-import 'package:first_app/my_math.dart';
 import 'package:flutter/material.dart';
+import 'package:first_app/instagram/main_screen.dart';
+import 'package:first_app/screens/media_query.dart';
+import 'package:first_app/screens/screen_one.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "ATEODINANG",
-      home: MyCinema(),
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MainScreen());
   }
 }
